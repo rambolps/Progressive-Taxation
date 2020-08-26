c56d85b44c50c948e42835fdca351969530b4996
Ryan Rambali <ryanrambali@gmail.com>
2020-08-26 18:37:46 -0400

    starting to create gui

--- Main.py
+++ Main.py
@@ -1,6 +1,33 @@
+#Import self-made resources
 import Graph as G
 import TaxCalculator as TC
 import Canada as C
+
+#import tkinter and ttkthemes for GUI
+import tkinter as tk
+import tkinter.ttk as ttk 
+from ttkthemes import ThemedStyle
+
+#create tkinter window
+app = tk.Tk()
+app.geometry("500x500")
+app.title("Progressive Taxation")
+
+#select the ttk style
+style = ThemedStyle(app)
+style.set_theme("breeze")
+
+Themed_Btn = ttk.Button(app,text='Themed button')
+Themed_Btn.pack()
+
+Themed_Scrollbar = ttk.Scrollbar(app,orient='vertical')
+Themed_Scrollbar.pack(side='right',fill='y')
+
+Themed_Entry = ttk.Entry(app)
+Themed_Entry.pack()
+
+#create app main loop (goes on forever, unless interuppted)
+app.mainloop()
 
 income = 0
 ftax = 0
